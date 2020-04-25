c8e0ab506d590c31671fceb020538a3b0b32dc93
Mounir Benabderrahmane <bnmounir7@gmail.com>
2020-04-25 16:51:25 -0400

    fix models superuser

--- profile_app/models.py
+++ profile_app/models.py
@@ -1,34 +1,31 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin
-from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin,
+                                        BaseUserManager)
 
 
 # Create your models here.
 class UserProfileManager(BaseUserManager):
-    '''
-    manager for user profiles
-    '''
+    """Manager for user profiles"""
     def create_user(self, email, name, password=None):
-        '''
-        create a new profile
-        '''
+        """Create a new user profile"""
         if not email:
-            raise ValueError('user must have an email')
+            raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(
+            email=email,
+            name=name,
+        )
 
         user.set_password(password)
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
 
-    def super_user(self, email, name, password):
-        '''
-        create admins
-        '''
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
         user = self.create_user(email, name, password)
+
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
@@ -58,4 +55,3 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
-
